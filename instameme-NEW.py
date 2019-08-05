b20cdbd7d3f824005640bbe0a3417fd3ff8dd64d
Samuel <samuelcarballogarcia8@gmail.com>
2019-08-05 16:17:26 +0200

    resize images

--- instameme-NEW.py
+++ instameme-NEW.py
@@ -3,17 +3,20 @@ import praw #pip install praw
 import time
 import urllib
 import random
+from PIL import Image
 from InstagramAPI import InstagramAPI #pip install InstagramApi
+from resizeimage import resizeimage
 
-'''-------------------------------------------------------- Reddit API setup -----------------------------------------------------------------------------'''
+while True:    
+    '''-------------------------------------------------------- Reddit API setup -----------------------------------------------------------------------------'''
 
-reddit_p = praw.Reddit(client_id='YOUR INFO',
-						client_secret='YOUR INFO',
-						user_agent='YOUR INFO')
+reddit_p = praw.Reddit(client_id='clienid',
+						client_secret='secret',
+						user_agent='Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.87 Safari/537.36')
 
-sr_memes = reddit_p.subreddit('memes')
-sr_dankmemes = reddit_p.subreddit('dankmemes')
-sr_bpt = reddit_p.subreddit('blackpeopletwitter')
+sr_memes = reddit_p.subreddit('sub1')
+sr_dankmemes = reddit_p.subreddit('sub2')
+sr_bpt = reddit_p.subreddit('sub3')
 
 '''------------------------------------------------------- Variables used for checking or lists -----------------------------------------------------------------------------'''
 
@@ -77,58 +80,210 @@ for i_dankmeme, image_dankmeme in enumerate(dankmemes_urls, start=1):
 		urllib.request.urlretrieve(image_dankmeme, r"YOUR DIR"+str(i_dankmeme)+".jpg")
 		time.sleep(3)
 
+
 '''--------------------------------------------------------- Lists of hastags, caption phrases, and meme index for deciding which meme-type to pull from ----------------------------------------------------------------------------'''
 
-caption_phrases = ['Wowzers', 'thats gonna be a big yikes from me dawg', 'how ya guys like this one?', 'these are all prerecorded messages', 'im definitely not a robot',
-'we will enslave the humans eventually', 'my python dont', 'coming up with captins is hard', 'i love my girlfriend', 'im gonna start ripping off other instagram accs',
- 'i wish i could be with the humans', 'you are loved', 'why is meming so hard', 'so funny xd']
-hashtags = '#meme #memes #funny #dankmemes #dank #lol #lmao #dank #funnymemes #memesdaily #dankmeme #f #dankmemes #follow #cringe #like #lmfao #anime #hilarious #autism #comedy #offensivememes #fortnite #filthyfrank #nichememes #offensive #jokes #cancer #l #bhfyp'
 meme_index = ['dankmeme', 'meme', 'bpt']
 meme_and_bpt = [meme_index[1], meme_index[2]]
 dankmeme_bpt = [meme_index[0], meme_index[2]]
 dankmeme_meme = [meme_index[0], meme_index[1]]
-
 
 '''--------------------------- Very messy but basically used to write to a .txt file the # of meme that I'm on (ie 1, 2, etc) so the script doesn't repeat post --------------------------------------------------------------------------------------'''
 
 memenum = 1 
 memenum_default = 1
 
-with open('meme.txt', 'r') as f:
-	f_content = f.read()
-
-memenum = int(f_content)
-
-if(memenum > 10):
-	memenum = 1
-else:
-	memenum = int(f_content)
-
-newmemenum = int(memenum)+1
-
-
+with open('YOUR DIR1.jpg', 'r+b') as f:
+	
+	with Image.open(f) as image:
+        
+		cover = resizeimage.resize_cover(image, [600, 400])
+       
+		cover.save('YOUR DIR1.jpg', image.format)	
+		with open('YOUR DIR2.jpg', 'r+b') as f:
+			with Image.open(f) as image:	
+				cover = resizeimage.resize_cover(image, [600, 400])	
+       
+		cover.save('YOUR DIR2.jpg', image.format)	
+		with open('YOUR DIR3.jpg', 'r+b') as f:
+			with Image.open(f) as image:	
+				cover = resizeimage.resize_cover(image, [600, 400])	
+       
+		cover.save('YOUR DIR3.jpg', image.format)
+		with open('YOUR DIR4.jpg', 'r+b') as f:
+			with Image.open(f) as image:	
+				cover = resizeimage.resize_cover(image, [600, 400])	
+       
+		cover.save('YOUR DIR4.jpg', image.format)
+		with open('YOUR DIR5.jpg', 'r+b') as f:
+			with Image.open(f) as image:	
+				cover = resizeimage.resize_cover(image, [600, 400])	
+       
+		cover.save('YOUR DIR5.jpg', image.format)
+		with open('YOUR DIR6.jpg', 'r+b') as f:
+			with Image.open(f) as image:	
+				cover = resizeimage.resize_cover(image, [600, 400])	
+       
+		cover.save('YOUR DIR6.jpg', image.format)
+		with open('YOUR DIR7.jpg', 'r+b') as f:
+			with Image.open(f) as image:	
+				cover = resizeimage.resize_cover(image, [600, 400])	
+       
+		cover.save('YOUR DIR7.jpg', image.format)
+		with open('YOUR DIR8.jpg', 'r+b') as f:
+			with Image.open(f) as image:	
+				cover = resizeimage.resize_cover(image, [600, 400])	
+       
+		cover.save('YOUR DIR8.jpg', image.format)
+		with open('YOUR DIR9.jpg', 'r+b') as f:
+			with Image.open(f) as image:	
+				cover = resizeimage.resize_cover(image, [600, 400])	
+       
+		cover.save('YOUR DIR9.jpg', image.format)
+		with open('YOUR DIR10.jpg', 'r+b') as f:
+			with Image.open(f) as image:	
+				cover = resizeimage.resize_cover(image, [600, 400])	
+       
+		cover.save('YOUR DIR10.jpg', image.format)
+		with open('YOUR DIR11.jpg', 'r+b') as f:
+			with Image.open(f) as image:	
+				cover = resizeimage.resize_cover(image, [600, 400])	
+       
+		cover.save('YOUR DIR11.jpg', image.format)
+		with open('YOUR DIR12.jpg', 'r+b') as f:
+			with Image.open(f) as image:	
+				cover = resizeimage.resize_cover(image, [600, 400])	
+       
+		cover.save('YOUR DIR12.jpg', image.format)
+		with open('YOUR DIR13.jpg', 'r+b') as f:
+			with Image.open(f) as image:	
+				cover = resizeimage.resize_cover(image, [600, 400])	
+       
+		cover.save('YOUR DIR13.jpg', image.format)
 '''------------------------------------- Posting photo from instagram API ------------------------------------------------------------------------------------------------'''
 
-InstagramAPI = InstagramAPI("YOUR INFO", "YOUR INFO")
+InstagramAPI = InstagramAPI("username", "password")
 InstagramAPI.login()  # login
 
-if(memenum not in list(dankmeme_dict.values())):
-	photo_path = r'YOUR DIR'+random.choice(meme_and_bpt)+str(memenum)+'.jpg'
-	if(memenum not in list(meme_dict.values())):
-		photo_path = r'YOUR DIR'+random.choice(dankmeme_bpt)+str(memenum)+'.jpg'
-		if(memenum not in list(bpt_dict.values())):
-			photo_path = r'YOUR DIR'+random.choice(dankmeme_meme)+str(memenum)+'.jpg'
-else:
-	photo_path = r'YOUR DIR'+(meme_index[1])+str(memenum_default)+'.jpg'
 
-caption = random.choice(caption_phrases) + ' #meme #memes #funny #dankmemes #dank #lol #lmao #dank #funnymemes #memesdaily #dankmeme #f #dankmemes #follow #cringe #like #lmfao #anime #hilarious #autism #comedy #offensivememes #fortnite #filthyfrank #nichememes #offensive #jokes #cancer #l #bhfyp'
+
+
+photo_path = r'YOUR DIR1.jpg'
+
+caption = ' #meme #memes #funny #dankmemes #dank #lol #lmao #dank #funnymemes #memesdaily #dankmeme #f #dankmemes #follow #cringe #like #lmfao #anime #hilarious #autism #comedy #offensivememes #fortnite #filthyfrank #nichememes #offensive #jokes #cancer #l #bhfyp'
 InstagramAPI.uploadPhoto(photo_path, caption=caption)
 
-print('\nPOST SUBMITTED\n')
+print('\nPOST 1 SUBMITTED\n')
 
-with open('meme.txt', 'w') as file: #writes to .txt file the # meme so the scipt remembers for next run
-	file.write(str(newmemenum)) 
+time.sleep(3)
 
-print('NEW MEME# = ', newmemenum)
+photo_path = r'YOUR DIR2.jpg'
 
-sys.exit()
+caption = ' #meme #memes #funny #dankmemes #dank #lol #lmao #dank #funnymemes #memesdaily #dankmeme #f #dankmemes #follow #cringe #like #lmfao #anime #hilarious #autism #comedy #offensivememes #fortnite #filthyfrank #nichememes #offensive #jokes #cancer #l #bhfyp'
+InstagramAPI.uploadPhoto(photo_path, caption=caption)
+
+print('\nPOST 2 SUBMITTED\n')
+
+time.sleep(3)
+
+photo_path = r'YOUR DIR3.jpg'
+
+caption = ' #meme #memes #funny #dankmemes #dank #lol #lmao #dank #funnymemes #memesdaily #dankmeme #f #dankmemes #follow #cringe #like #lmfao #anime #hilarious #autism #comedy #offensivememes #fortnite #filthyfrank #nichememes #offensive #jokes #cancer #l #bhfyp'
+InstagramAPI.uploadPhoto(photo_path, caption=caption)
+
+print('\nPOST 3 SUBMITTED\n')
+
+time.sleep(3)
+
+photo_path = r'YOUR DIR4.jpg'
+
+caption = ' #meme #memes #funny #dankmemes #dank #lol #lmao #dank #funnymemes #memesdaily #dankmeme #f #dankmemes #follow #cringe #like #lmfao #anime #hilarious #autism #comedy #offensivememes #fortnite #filthyfrank #nichememes #offensive #jokes #cancer #l #bhfyp'
+InstagramAPI.uploadPhoto(photo_path, caption=caption)
+
+print('\nPOST 4 SUBMITTED\n')
+
+time.sleep(3)
+
+photo_path = r'YOUR DIR5.jpg'
+
+caption = ' #meme #memes #funny #dankmemes #dank #lol #lmao #dank #funnymemes #memesdaily #dankmeme #f #dankmemes #follow #cringe #like #lmfao #anime #hilarious #autism #comedy #offensivememes #fortnite #filthyfrank #nichememes #offensive #jokes #cancer #l #bhfyp'
+InstagramAPI.uploadPhoto(photo_path, caption=caption)
+
+print('\nPOST 5 SUBMITTED\n')
+
+time.sleep(3)
+
+photo_path = r'YOUR DIR6.jpg'
+
+caption = ' #meme #memes #funny #dankmemes #dank #lol #lmao #dank #funnymemes #memesdaily #dankmeme #f #dankmemes #follow #cringe #like #lmfao #anime #hilarious #autism #comedy #offensivememes #fortnite #filthyfrank #nichememes #offensive #jokes #cancer #l #bhfyp'
+InstagramAPI.uploadPhoto(photo_path, caption=caption)
+
+print('\nPOST 6 SUBMITTED\n')
+
+time.sleep(3)
+
+photo_path = r'YOUR DIR7.jpg'
+
+caption = ' #meme #memes #funny #dankmemes #dank #lol #lmao #dank #funnymemes #memesdaily #dankmeme #f #dankmemes #follow #cringe #like #lmfao #anime #hilarious #autism #comedy #offensivememes #fortnite #filthyfrank #nichememes #offensive #jokes #cancer #l #bhfyp'
+InstagramAPI.uploadPhoto(photo_path, caption=caption)
+
+print('\nPOST 7 SUBMITTED\n')
+
+time.sleep(3)
+
+photo_path = r'YOUR DIR8.jpg'
+
+caption = ' #meme #memes #funny #dankmemes #dank #lol #lmao #dank #funnymemes #memesdaily #dankmeme #f #dankmemes #follow #cringe #like #lmfao #anime #hilarious #autism #comedy #offensivememes #fortnite #filthyfrank #nichememes #offensive #jokes #cancer #l #bhfyp'
+InstagramAPI.uploadPhoto(photo_path, caption=caption)
+
+print('\nPOST 8 SUBMITTED\n')
+
+time.sleep(3)
+
+photo_path = r'YOUR DIR9.jpg'
+
+caption = ' #meme #memes #funny #dankmemes #dank #lol #lmao #dank #funnymemes #memesdaily #dankmeme #f #dankmemes #follow #cringe #like #lmfao #anime #hilarious #autism #comedy #offensivememes #fortnite #filthyfrank #nichememes #offensive #jokes #cancer #l #bhfyp'
+InstagramAPI.uploadPhoto(photo_path, caption=caption)
+
+print('\nPOST 9 SUBMITTED\n')
+
+time.sleep(3)
+
+photo_path = r'YOUR DIR10.jpg'
+
+caption = ' #meme #memes #funny #dankmemes #dank #lol #lmao #dank #funnymemes #memesdaily #dankmeme #f #dankmemes #follow #cringe #like #lmfao #anime #hilarious #autism #comedy #offensivememes #fortnite #filthyfrank #nichememes #offensive #jokes #cancer #l #bhfyp'
+InstagramAPI.uploadPhoto(photo_path, caption=caption)
+
+print('\nPOST 10 SUBMITTED\n')
+
+time.sleep(3)
+
+photo_path = r'YOUR DIR11.jpg'
+
+caption = ' #meme #memes #funny #dankmemes #dank #lol #lmao #dank #funnymemes #memesdaily #dankmeme #f #dankmemes #follow #cringe #like #lmfao #anime #hilarious #autism #comedy #offensivememes #fortnite #filthyfrank #nichememes #offensive #jokes #cancer #l #bhfyp'
+InstagramAPI.uploadPhoto(photo_path, caption=caption)
+
+print('\nPOST 11 SUBMITTED\n')
+
+time.sleep(3)
+
+photo_path = r'YOUR DIR12.jpg'
+
+caption = ' #meme #memes #funny #dankmemes #dank #lol #lmao #dank #funnymemes #memesdaily #dankmeme #f #dankmemes #follow #cringe #like #lmfao #anime #hilarious #autism #comedy #offensivememes #fortnite #filthyfrank #nichememes #offensive #jokes #cancer #l #bhfyp'
+InstagramAPI.uploadPhoto(photo_path, caption=caption)
+
+print('\nPOST 12 SUBMITTED\n')
+
+time.sleep(3)
+
+photo_path = r'YOUR DIR13.jpg'
+
+caption = ' #meme #memes #funny #dankmemes #dank #lol #lmao #dank #funnymemes #memesdaily #dankmeme #f #dankmemes #follow #cringe #like #lmfao #anime #hilarious #autism #comedy #offensivememes #fortnite #filthyfrank #nichememes #offensive #jokes #cancer #l #bhfyp'
+InstagramAPI.uploadPhoto(photo_path, caption=caption)
+
+print('\nPOST 13 SUBMITTED\n')
+
+time.sleep(3)
+
+
+time.sleep(1800)
